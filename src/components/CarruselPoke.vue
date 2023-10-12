1d0b4bb44fde7bab2789447a3d6d431a6c523fbe
Bryalejo <brayanalejo79@gmail.com>
2023-10-12 18:36:52 -0500

    correccion carrusel validacion

--- src/components/CarruselPoke.vue
+++ src/components/CarruselPoke.vue
@@ -7,38 +7,89 @@
       :pagination="{
         clickable: true,
       }"
-      class="mySwiper h-full my-20 mx-20  text-xl "
+      class="mySwiper h-full my-20 mx-20 text-xl"
     >
       <swiper-slide v-for="pokemon in pokemonList" :key="pokemon.name">
-        <img src="/imgs/pokemon1.png" alt="" class=" py-5 w-full" />
+        <img src="/imgs/pokemon1.png" alt="" class="py-5 w-full" />
         <div class="ml-5">
           Nombre: <span class="">{{ pokemon.name }}</span> <br />
-          <button @click="showPokemonDetails(pokemon)" class="bg-red-500 shadow-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-yellow-500 duration-300 text-white shadow-red-500/50 rounded-md px-2 outline outline-offset-2 outline-red-600 my-5 " type="button">
+          <button
+            @click="showPokemonDetails(pokemon)"
+            class="bg-red-500 shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-500 duration-300 text-white shadow-red-500/50 rounded-md px-2 outline outline-offset-2 outline-red-600 my-5"
+            type="button"
+          >
             Detalles
           </button>
         </div>
       </swiper-slide>
     </swiper>
 
-    <Modal :size="size" v-if="isShowModal" @close="closeModal">
+    <Modal v-if="isShowModal" @close="closeModal">
       <template #header>
         <div class="flex items-center text-4xl text-white">
-         {{ selectedPokemon.name }}
+          {{ selectedPokemon.name }}
         </div>
       </template>
       <template #body>
         <!-- Use the PokemonDetails component here -->
         <div class="text-white font-bold text-xl">
           <ul>
-            <li>Indice de felicidad: {{ selectedPokemon.additionalInfo.baseHappiness }} </li>
-            <li>Tasa de Captura: {{selectedPokemon.additionalInfo.captureRate}} </li>
-            <li>Color: {{selectedPokemon.additionalInfo.color.name}} </li>
-            <li>Tipo: {{selectedPokemon.additionalInfo.eggGroups[1].name}} </li>
-            <li v-if="selectedPokemon.additionalInfo.evolvesFromSpecies && selectedPokemon.additionalInfo.evolvesFromSpecies.name">
-              Pokemon pre-evolucion: {{ selectedPokemon.additionalInfo.evolvesFromSpecies.name }}</li>
-              <li v-else>
-                Evolucion: Pokemon primario
-              </li>
+            <li>
+              Indice de felicidad:
+              {{ selectedPokemon.additionalInfo.baseHappiness }}
+            </li>
+            <li>
+              Tasa de Captura: {{ selectedPokemon.additionalInfo.captureRate }}
+            </li>
+            <li
+              v-if="
+                selectedPokemon &&
+                selectedPokemon.additionalInfo &&
+                selectedPokemon.additionalInfo.color &&
+                selectedPokemon.additionalInfo.color.name
+              "
+            >
+              Color: {{ selectedPokemon.additionalInfo.color.name }}
+            </li>
+            <li v-else>Color: No se encontró información de color.</li>
+            <li
+              v-if="
+                selectedPokemon &&
+                selectedPokemon.additionalInfo &&
+                selectedPokemon.additionalInfo.eggGroups &&
+                selectedPokemon.additionalInfo.eggGroups[0] &&
+                selectedPokemon.additionalInfo.eggGroups[0].name" 
+            >
+              Especie:{{ selectedPokemon.additionalInfo.eggGroups[0].name }}
+             
+            </li>
+            <li v-else>Tipo: null</li>
+            <li
+              v-if="
+                selectedPokemon &&
+                selectedPokemon.additionalInfo &&
+                selectedPokemon.additionalInfo.eggGroups &&
+                selectedPokemon.additionalInfo.eggGroups[1] &&
+                selectedPokemon.additionalInfo.eggGroups[1].name 
+              "
+            >
+              
+              Tipo: {{ selectedPokemon.additionalInfo.eggGroups[1].name  }}
+            </li>
+            <li v-else>Tipo: null</li>
+
+            <li
+              v-if="
+                selectedPokemon &&
+                selectedPokemon.additionalInfo &&
+                selectedPokemon.additionalInfo.evolvesFromSpecies &&
+                selectedPokemon.additionalInfo.evolvesFromSpecies.name
+              "
+            >
+              Pokemon pre-evolucion:
+              {{ selectedPokemon.additionalInfo.evolvesFromSpecies.name }}
+            </li>
+            <li v-else>Evolucion: Pokemon primario</li>
           </ul>
         </div>
       </template>
@@ -51,7 +102,7 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination } from "swiper/modules";
-import { Modal } from 'flowbite-vue';
+import { Modal } from "flowbite-vue";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -60,7 +111,7 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
-    Modal
+    Modal,
   },
   setup() {
     const pokemonList = ref([]);
@@ -73,7 +124,7 @@ export default {
         .then(async (response) => {
           const pokemonSpeciesList = response.data.results;
 
-          // Realizar solicitudes adicionales para obtener información adicional
+        
           const promises = pokemonSpeciesList.map(async (pokemon) => {
             const response = await axios.get(pokemon.url);
             const additionalInfo = {
@@ -89,12 +140,15 @@ export default {
             };
           });
 
-          // Esperar a que se completen todas las solicitudes adicionales
+         
           const pokemonListWithInfo = await Promise.all(promises);
           pokemonList.value = pokemonListWithInfo;
         })
         .catch((error) => {
-          console.error("Error al cargar la lista de Pokémon desde la API", error);
+          console.error(
+            "Error al cargar la lista de Pokémon desde la API",
+            error
+          );
         });
     };
 
@@ -119,9 +173,8 @@ export default {
       isShowModal,
       selectedPokemon,
       closeModal,
-      showPokemonDetails
+      showPokemonDetails,
     };
   },
 };
 </script>
-
